b0b03e31a8a32be6175ea7be1d3d2bcbe84442d4
Tyler Jang <tyler@trunk.io>
2023-08-07 15:23:34 -0700

    link checker WIP

---
+++ .trunk/link-checker/checker.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import re
+import yaml
+from pathlib import Path
+
+EXCLUDED_DIRS = ".git", ".trunk"
+LINK_REGEX = r'\[(?P<display>[^\]]+)\]\((?P<link>[^ ]+)\)'
+
+# Load the set of redirects from the gitbook config file
+with open(".gitbook.yaml") as stream:
+    config = yaml.safe_load(stream)
+
+redirects = config["redirects"]
+
+def validate_link(path, url):
+    return False
+
+# Iterate through all markdown files and check links
+for path in Path('.').rglob('*.md'):
+    print(path.name)
+    with open(path, "r") as file:
+        matches = re.findall(LINK_REGEX, file.read())
+        if len(matches) == 0:
+            continue
+        for match in matches:
+          if not validate_link(path, match[1]):
+              print("{}: Invalid url: '{}' with description: '{}'".format(path, match[1], match[0]))
+
+
