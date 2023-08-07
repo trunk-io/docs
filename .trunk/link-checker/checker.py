83f63def1ff611a9d94383e346b4e9e6f85252e0
Tyler Jang <tyler@trunk.io>
2023-08-07 16:24:21 -0700

    finish, cleanup

--- .trunk/link-checker/checker.py
+++ .trunk/link-checker/checker.py
@@ -6,8 +6,11 @@ import yaml
 from pathlib import Path
 from urllib.parse import urlparse
 
+# TODO: Create maps of frequently read files to improve performance
+# TODO: Parallize for performance
+
 LINK_REGEX = r'\[(?P<display>[^\]]+)\]\((?P<link>[^ \)]+)\)'
-ALLOW_LIST_URLS = ["https://marketplace.visualstudio.com/items?itemName=trunk.io", "https://static.trunk.io/assets/vscode\_init\_trunk.png", "https://static.trunk.io/assets/vscode\_side\_panel.png", "https://static.trunk.io/assets/vscode\_hide\_explorer\_panel.jpg", "https://static.trunk.io/assets/vscode\_doc\_links.png", "https://static.trunk.io/assets/vscode\_ignore\_issue.gif"]
+ALLOW_LIST_URLS = ["https://marketplace.visualstudio.com/items?itemName=trunk.io", "https://static.trunk.io/assets/vscode\_init\_trunk.png", "https://static.trunk.io/assets/vscode\_side\_panel.png", "https://static.trunk.io/assets/vscode\_hide\_explorer\_panel.jpg", "https://static.trunk.io/assets/vscode\_doc\_links.png", "https://static.trunk.io/assets/vscode\_ignore\_issue.gif", "mailto:support@trunk.io", "mailto:sales@trunk.io"]
 ALLOW_LIST_FRAGMENTS = ["sample"]
 
 # Load the set of redirects from the gitbook config file
@@ -20,7 +23,7 @@ fail = False
 def check_fragment(source_path, link_dest, fragment):
     if fragment in ALLOW_LIST_FRAGMENTS:
         return True
-    fragment_regex = r"# {}".format(fragment.replace("-", "."))
+    fragment_regex = r"# [`*_]*{}".format(fragment.replace("-", "."))
     with open(link_dest, "r") as target:
         if re.search(fragment_regex, target.read(), re.IGNORECASE) is None:
             print(">>> Could not detect fragment '{}' in file {} pointing to from {}".format(fragment, link_dest, source_path))
@@ -42,7 +45,8 @@ def validate_link(path, url):
         from_file_path = path.parent.joinpath(filename)
         from_root_path = filename
 
-        # print(url, fragment_index, from_file_path, os.path.exists(from_file_path), from_root_path, os.path.exists(from_root_path))
+        if filename == "." or filename == "./":
+            return check_fragment(path, path.parent.joinpath("README.md"), fragment)
         if os.path.exists(from_file_path):
             return check_fragment(path, from_file_path, fragment)
         if os.path.exists(from_root_path):
@@ -55,9 +59,7 @@ def validate_link(path, url):
     return False
 
 # Iterate through all markdown files and check links
-# for path in Path('.').rglob('*.md'):
-for path in Path('check').rglob('*.md'):
-    # print(path.name)
+for path in Path('.').rglob('*.md'):
     with open(path, "r") as file:
         matches = re.findall(LINK_REGEX, file.read())
         if len(matches) == 0:
@@ -66,7 +68,6 @@ for path in Path('check').rglob('*.md'):
           if not validate_link(path, match[1]):
               fail = True
               print("{}: Could not find url: '{}' with description: '{}'".format(path, match[1], match[0]))
-
 
 if fail:
     exit(1)
